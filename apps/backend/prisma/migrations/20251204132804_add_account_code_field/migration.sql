4e998654e147fcd62d6d2531a2c7e902653c7609
Fernando Moreira Costa <nandomoreira.me@gmail.com>
2025-12-05 08:22:59 -0300

    🐛 fix(prisma): remove duplicate migration for account code field

--- apps/backend/prisma/migrations/20251204132804_add_account_code_field/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "bank_account" ADD COLUMN "code" TEXT;
-
--- CreateIndex
-CREATE UNIQUE INDEX "bank_account_code_key" ON "bank_account"("code");
-
--- CreateIndex
-CREATE INDEX "bank_account_code_idx" ON "bank_account"("code");
-
